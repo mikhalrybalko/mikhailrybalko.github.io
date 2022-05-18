da3c252c7a193e13b7cd6036cffbf55fe1ef6f49
mikhalrybalko <98716807+mikhalrybalko@users.noreply.github.com>
2022-05-18 09:18:22 +0800

    Add files via upload

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -6,3 +6,25 @@ def is_prime(n):
     return True 
 print(is_prime(1))
 print(is_prime(20))
+
+
+def primes_below(n):
+    a=  []
+    for l in range(1,n):
+        if is_prime(l):
+            a.append(l)
+    return(a)   
+print(primes_below(100))
+
+
+
+a=input('enter a string :')# palindrome in string
+b=a[::-1]
+if a==b:
+    print(a,'is a palindrome')
+else:
+    print(a,'is not a palindrome')
+    print('a is not equal to b')
+if a!=b:
+    print(b, 'the reverse of', a)
+#output:
